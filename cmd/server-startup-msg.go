404702c55b02f13bf8acde99c0e49c94c50c7207
Zhengjiaao <zhengja@dist.com.cn>
2021-05-07 11:11:08 +0800

    update msg

--- cmd/server-startup-msg.go
+++ cmd/server-startup-msg.go
@@ -175,18 +175,18 @@ func printEventNotifiers() {
 // and custom platform specific message.
 func printCLIAccessMsg(endPoint string, alias string) {
 	// Get saved credentials.
-	cred := globalActiveCred
+	//cred := globalActiveCred
 
 	// Configure 'mc', following block prints platform specific information for minio client.
 	if color.IsTerminal() && !globalCLIContext.Anonymous {
 		//logStartupMessage(color.Blue("\nCommand-line Access: ") + mcQuickStartGuide)
 		if runtime.GOOS == globalWindowsOSName {
-			mcMessage := fmt.Sprintf("$ mc.exe alias set %s %s %s %s", alias,
-				endPoint, cred.AccessKey, cred.SecretKey)
+			//mcMessage := fmt.Sprintf("$ mc.exe alias set %s %s %s %s", alias,
+			//endPoint, cred.AccessKey, cred.SecretKey)
 			//logStartupMessage(fmt.Sprintf(getFormatStr(len(mcMessage), 3), mcMessage))
 		} else {
-			mcMessage := fmt.Sprintf("$ mc alias set %s %s %s %s", alias,
-				endPoint, cred.AccessKey, cred.SecretKey)
+			//mcMessage := fmt.Sprintf("$ mc alias set %s %s %s %s", alias,
+			//endPoint, cred.AccessKey, cred.SecretKey)
 			//logStartupMessage(fmt.Sprintf(getFormatStr(len(mcMessage), 3), mcMessage))
 		}
 	}
